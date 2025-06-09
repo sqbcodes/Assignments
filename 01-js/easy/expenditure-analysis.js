74296fb898571c91e50bbb17d9d33c9296377fb1
sqbcodes <kalessaqib@gmail.com>
2025-06-09 23:33:48 +0530

    expenditure

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,22 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+    const categoryTotals = {};
+
+    transactions.forEach(transaction => {
+        const { category, price } = transaction;
+
+        if (!categoryTotals[category]) {
+            categoryTotals[category] = 0;
+        }
+
+        categoryTotals[category] += price;
+    });
+
+    return Object.entries(categoryTotals).map(([category, totalSpent]) => ({
+        category,
+        totalSpent
+    }));
 }
 
 module.exports = calculateTotalSpentByCategory;
